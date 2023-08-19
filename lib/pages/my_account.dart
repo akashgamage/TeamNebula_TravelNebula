d3968efb5d18e999ede5909ab456015c7fe421d1
NSachini <sachini.anjana619@gmail.com>
2023-08-19 19:28:05 +0530

    Update

--- lib/pages/my_account.dart
+++ lib/pages/my_account.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
 
 class MyAccountPage extends StatelessWidget {
-  const MyAccountPage({super.key});
+  const MyAccountPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: Center(
-        child: Text(
-          "My Account",
-          style: TextStyle(
-            color: Color.fromARGB(255, 0, 0, 0),
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "My Account",
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20), // Adding some spacing
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Navigate back when the button is pressed
+              },
+              child: const Text("Back"),
+            ),
+          ],
         ),
-      ),
-    );
-  }
+     ),
+);
+}
 }
 
-// Demo --------------------------
+// demo -------------------------------------------------------------------------------------
--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 
 class NotificationsPage extends StatelessWidget {
-  const NotificationsPage({super.key});
+  const NotificationsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: Center(
-        child: Text(
-          "Notifications",
-          style: TextStyle(
-            color: Color.fromARGB(255, 0, 0, 0),
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Notifications",
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20), // Adding some spacing
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Navigate back when the button is pressed
+              },
+              child: const Text("Back"),
+            ),
+          ],
         ),
       ),
     );
